81158d8d5f49abef4949fe6ce64fee553462182f
Mohamed-Ismail-Dev <mhdismail.er@gmail.com>
2025-09-15 18:08:48 +0000

    Update Terraform project: multiple instances, remote backend, outputs

---
+++ aws/remote_state/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_id    = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  # With account id, this S3 bucket names can be *globally* unique.
+  bucket = "${local.account_id}-terraform-states"
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PROVISIONED"
+  read_capacity = 1
+  write_capacity = 1
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
